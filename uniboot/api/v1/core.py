1e19b579ed769a9006bfb9c0e3710851e9f8592d
Myst33d <notmyst33d@gmail.com>
2023-03-02 18:34:26 +0600

    Disable AVB in DTB fstab

--- uniboot/api/v1/core.py
+++ uniboot/api/v1/core.py
@@ -24,6 +24,12 @@ async def post_api_v1_patch(request: web.Request):
     except:
         return web.json_response({"status": "error", "message": "Boot image was not sent"}, status=400)
 
+    # Create random id (like Magisk does)
+    random_id = "".join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(5))
+
+    # Get filename
+    filename = boot.filename.split(".")[0] + "_patched_" + random_id + ".img"
+
     # Create temporary directory
     temp = tempfile.mkdtemp()
 
@@ -96,19 +102,44 @@ async def post_api_v1_patch(request: web.Request):
             shutil.rmtree(temp)
             return web.json_response({"status": "error", "message": "Unknown error occurred"}, status=500)
 
-    # Search for fstabs and disable AVB
-    for entry in os.listdir(temp + "/ramdisk/first_stage_ramdisk"):
-        if entry.startswith("fstab"):
-            with open(temp + "/ramdisk/first_stage_ramdisk/" + entry, "r") as f:
-                data = f.read() \
-                    .replace(",avb_keys=/avb/q-gsi.avbpubkey:/avb/r-gsi.avbpubkey:/avb/s-gsi.avbpubkey", "") \
-                    .replace(",avb=vbmeta_system_ext", "") \
-                    .replace(",avb=vbmeta_system", "") \
-                    .replace(",avb=vbmeta_vendor", "") \
-                    .replace(",avb=vbmeta_product", "")
+    # Search for FSR fstabs and disable AVB
+    if os.path.isdir(f"{temp}/ramdisk/first_stage_ramdisk"):
+        for entry in os.listdir(f"{temp}/ramdisk/first_stage_ramdisk"):
+            if entry.startswith("fstab"):
+                with open(f"{temp}/ramdisk/first_stage_ramdisk/{entry}", "r") as f:
+                    data = f.read() \
+                        .replace(",avb_keys=/avb/q-gsi.avbpubkey:/avb/r-gsi.avbpubkey:/avb/s-gsi.avbpubkey", "") \
+                        .replace(",avb=vbmeta_system_ext", "") \
+                        .replace(",avb=vbmeta_system", "") \
+                        .replace(",avb=vbmeta_vendor", "") \
+                        .replace(",avb=vbmeta_product", "")
 
-            with open(temp + "/ramdisk/first_stage_ramdisk/" + entry, "w") as f:
-                f.write(data)
+                with open(f"{temp}/ramdisk/first_stage_ramdisk/{entry}", "w") as f:
+                    f.write(data)
+
+    # Search for AVB in DTB and disable it
+    for dtb_file in ["kernel_dtb", "dtb"]:
+        if os.path.isfile(f"{temp}/{dtb_file}"):
+            async with aiofiles.open(f"{temp}/{dtb_file}", "rb") as file:
+                data = bytearray(await file.read())
+                data_len = len(data)
+
+            for index in range(data_len):
+                if data[index:index + 9] == b",avb_keys":
+                    null_terminator_index = index
+
+                    while data[null_terminator_index] != 0 and null_terminator_index < data_len:
+                        null_terminator_index += 1
+
+                    if null_terminator_index != data_len:
+                        for avb_keys_index in range(index, null_terminator_index):
+                            data[avb_keys_index] = b" "[0]
+                elif data[index:index + 4] == b",avb":
+                    for avb_index in range(index, index + 4):
+                        data[avb_index] = b" "[0]
+
+            async with aiofiles.open(f"{temp}/{dtb_file}", "wb") as file:
+                await file.write(data)
 
     # Repack ramdisk.cpio
     try:
@@ -141,7 +172,7 @@ async def post_api_v1_patch(request: web.Request):
     # Repack boot.img
     try:
         await (await asyncio.create_subprocess_exec(
-            request.app["config"]["bin"] + "/magiskboot", "repack", "boot.img", "uniboot.img",
+            request.app["config"]["bin"] + "/magiskboot", "repack", "boot.img", filename,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
             cwd=temp
@@ -150,12 +181,9 @@ async def post_api_v1_patch(request: web.Request):
         shutil.rmtree(temp)
         return web.json_response({"status": "error", "message": "Unknown error occurred"}, status=500)
 
-    # Create random id (like Magisk does)
-    random_id = "".join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(5))
-
     # Create response with patched boot.img
-    async with aiofiles.open(temp + "/uniboot.img", "rb") as f:
-        response = web.Response(body=await f.read(), headers={"Content-Disposition": f"filename=uniboot_{random_id}.img"})
+    async with aiofiles.open(f"{temp}/{filename}", "rb") as f:
+        response = web.Response(body=await f.read(), headers={"Content-Disposition": f"filename={filename}"})
 
     # Cleanup
     shutil.rmtree(temp)
